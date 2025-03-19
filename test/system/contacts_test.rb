840c2022880bd3fa7afdd63d6dc23336d80678c4
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-19 18:55:12 -0400

    Add test for unauthenticated Contact route redirect

--- test/system/contacts_test.rb
+++ test/system/contacts_test.rb
@@ -3,9 +3,18 @@ require "application_system_test_case"
 class ContactsTest < ApplicationSystemTestCase
   setup do
     @user = users(:one)
-    sign_in @user
-
     @contact = contacts(:two)
+
+    sign_in @user
+  end
+
+  test "unauthenticated user is redirected to sign in" do
+    sign_out @user  # Manually sign out for this specific test
+    visit contacts_url
+
+    # Ensure we are redirected to the sign-in page
+    assert_current_path new_user_session_path
+    assert_text "You need to sign in or sign up"
   end
 
   test "visiting the index" do

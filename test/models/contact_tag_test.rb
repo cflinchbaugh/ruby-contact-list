10cd557e986c9b297db092a3933c701e2afc9388
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-21 20:12:24 -0400

    Test models

--- test/models/contact_tag_test.rb
+++ test/models/contact_tag_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ContactTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @contact = contacts(:one)
+    @tag = tags(:one)
+    @contact_tag = ContactTag.new(contact: @contact, tag: @tag)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @contact_tag.valid?
+  end
+
+  test "should require a contact" do
+    @contact_tag.contact = nil
+    assert_not @contact_tag.valid?
+  end
+
+  test "should require a tag" do
+    @contact_tag.tag = nil
+    assert_not @contact_tag.valid?
+  end
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @contact = contacts(:one)
+  end
+
+  test "should be valid with all attributes and user" do
+    assert @contact.valid?
+  end
+
+  test "should require a user" do
+    @contact.user = nil
+    assert_not @contact.valid?
+  end
+
+  test "should have many contact_tags" do
+    assert_respond_to @contact, :contact_tags
+  end
+
+  test "should have many tags through contact_tags" do
+    assert_respond_to @contact, :tags
+  end
 end

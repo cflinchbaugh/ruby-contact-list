f6d3aa35e4db927bfdc93e08ff54505854bc6b09
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-21 20:25:57 -0400

    Restore correct_user validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
   before_action :authenticate_user!  # Require authentication for all actions
   before_action :set_contact, only: %i[show edit update destroy]
-  # before_action :correct_user, only: [ :edit, :update, :destroy ]
+  before_action :correct_user, only: [ :edit, :update, :destroy ]
 
 
   # GET /contacts or /contacts.json

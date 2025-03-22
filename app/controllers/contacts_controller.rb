7541ad6c8c540cf9100802c17b2eaa3833fb332f
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-21 20:53:50 -0400

    Lint fixes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,23 +8,23 @@ class ContactsController < ApplicationController
   def index
     @page_size = 5
     @tags = Tag.all
-  
+
     @contacts = current_user.contacts
-  
+
     # Search by name
     if params[:search].present?
       query = "%#{params[:search]}%"
       @contacts = @contacts.where("first_name LIKE ? OR last_name LIKE ?", query, query)
     end
-  
+
     # Filter by tag
     if params[:tag_ids].present?
       @contacts = @contacts.joins(:tags).where(tags: { id: params[:tag_ids] }).distinct
     end
-  
+
     @contacts = @contacts.page(params[:page]).per(@page_size)
   end
-  
+
 
   # GET /contacts/1 or /contacts/1.json
   def show

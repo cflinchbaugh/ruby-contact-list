4e1340f21f665e76d4b7bf8ee8d37e259876be7d
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-26 19:28:22 -0400

    Update config to use local files during testing

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,15 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  if Rails.env.test?
+    storage :file
+  end
+
+  # optional: skip versions in test
+  if Rails.env.test?
+    def thumb; end
+  end
+
   # Optional: Define any transformation you want
   version :thumb do
     process resize_to_fill: [ 200, 200 ]

a3a9e10b33605e86c93f33a4740661658a97260d
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-26 19:44:14 -0400

    Add clarifying comments in uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,8 +1,9 @@
 class AvatarUploader < CarrierWave::Uploader::Base
+  # Tests should not interact with CarrierWave directly, it is an acceptable practice to conditionally separate Uploaders/Mailers/Loggers as done here
   if Rails.env.test?
     storage :file
 
-    # Stub out `url` in test to point to a known local file path
+    # Stub out `url` in test to point to a known local file path rather than rely on a CarrierWave file
     def url(*args)
       "/test/fixtures/files/avatar.png"
     end

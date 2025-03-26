26112977e808cec611841474ed50fc681630c6eb
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-26 19:37:07 -0400

    Update avatar_uploader to avoid Cloudinary in tests

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,17 +1,17 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
-
   if Rails.env.test?
     storage :file
-  end
 
-  # optional: skip versions in test
-  if Rails.env.test?
-    def thumb; end
-  end
+    # Stub out `url` in test to point to a known local file path
+    def url(*args)
+      "/test/fixtures/files/avatar.png"
+    end
+  else
+    include Cloudinary::CarrierWave
 
-  # Optional: Define any transformation you want
-  version :thumb do
-    process resize_to_fill: [ 200, 200 ]
+    # Optional Cloudinary transformations
+    version :thumb do
+      process resize_to_fill: [ 200, 200 ]
+    end
   end
 end

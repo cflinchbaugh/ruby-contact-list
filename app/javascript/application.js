d159892b1eefcc10146a2243b188290a39c59f93
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-27 18:53:01 -0400

    Remove deprecated image inputs, rename where appropriate

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,5 +4,5 @@ import "controllers";
 
 // Custom code
 // Be sure to add to config/importmap.rb and restart the server to see the changes
-import "avatar_input_preview";
 import "filter_form_tracker";
+import "image_input_preview";

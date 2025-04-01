f33f9fe17c6e7ec6f3962c8d357cd92a5b90a50c
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-03-31 22:35:19 -0400

    Expose api/v1; successfully log in with postman

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
 
 // Custom code
-// Be sure to add to config/importmap.rb and restart the server to see the changes
+// IMPORTANT: Be sure to add to config/importmap.rb and restart the server to see the changes
 import "filter_form_tracker";
 import "image_input_preview";

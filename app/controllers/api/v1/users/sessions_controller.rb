880cf7f11b5d135a6452bd09c02a8bdd01ebd88b
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-04-01 20:05:36 -0400

    Code cleanup, remove excessive comment

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
-  respond_to :json # You can remove turbo_stream since we are doing full-page redirects now
+  respond_to :json
 
   def create
     user = User.find_by(email: params[:user][:email])
@@ -8,7 +8,6 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
     if user && user.valid_password?(params[:user][:password])
       sign_in(user)
 
-      # Redirect after successful login
       redirect_to root_path, notice: "Signed in successfully."
     else
       render json: { error: "Invalid email or password" }, status: :unauthorized
@@ -18,7 +17,6 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   def destroy
     sign_out(current_user)
 
-    # Redirect after successful sign out
     redirect_to root_path, notice: "Signed out successfully."
   end
 end
